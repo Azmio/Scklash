196781ac5eeda7952d599f99b7dd2eb9394c36a2
Frosty <aayushshukla11@gmail.com>
2021-06-23 20:50:43 +0530

    Utility State
    -Added a list to the enemy controller which will hold all the existing utility state enemies in the list
    -Added a bool to the enemyAI which will be set to true if the enemy is a utility state now
    -Added some helper functions for the health and the health bar
    -Fixed Health bar color and rotation

--- Assets/_Scripts/Enemy/EnemyAI.cs
+++ Assets/_Scripts/Enemy/EnemyAI.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public enum SpawnerState { Chasing, MeleeAttack, RangedAttack };
+    public enum State { Chasing, MeleeAttack, RangedAttack, Utility };
 
     public bool isRanged;
 
     public EnemyMovement enemyMovement;
 
     public EnemyCombat enemyCombat;
+
+    public bool isUtility = false;
+
+    public HealthScript enemyHealthSystem;
 
 
     public float attackRange;
@@ -19,7 +23,7 @@ public class EnemyAI : MonoBehaviour
     float distance;
 
     //Shows the current action
-    public SpawnerState currentAction;
+    public State currentAction;
 
     public float timeUntilPositionChange = 1.5f;
 
@@ -29,37 +33,74 @@ public class EnemyAI : MonoBehaviour
 
 
 
+
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         enemyMovement = this.gameObject.GetComponent<EnemyMovement>();
         enemyCombat = this.gameObject.GetComponent<EnemyCombat>();
+        enemyHealthSystem = this.gameObject.GetComponent<HealthScript>();
+    }
+
+    private void Start()
+    {
+        InitializeIfEnemy();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         AIStateController();
+        HealthBar();
+    }
+    void InitializeIfEnemy()
+    {
+        enemyHealthSystem.healthBarCanvas = this.transform.Find("Canvas").gameObject;
+        EnemySpawner.enemySpawner.enemiesInTheScene.Add(this);
     }
 
+    void ChangeToUtility()
+    {
+        enemyMovement.agent.velocity = Vector3.zero;
+        enemyMovement.enabled = false;
+        enemyCombat.enabled = false;
+        EnemySpawner.enemySpawner.enemiesInTheScene.Remove(this);
+        EnemySpawner.enemySpawner.UtilityStatesInTheScene.Add(this);
+        currentAction = State.Utility;
+        enemyHealthSystem.healthBarCanvas.SetActive(false);
+    }
 
+    void HealthBar()
+    {
+        enemyHealthSystem.healthBarCanvas.transform.rotation = Camera.main.transform.rotation;
+        enemyHealthSystem.healthSlider.targetGraphic.color = Color.Lerp(Color.red, Color.green, enemyHealthSystem.currentHealth / 100f);
+    }
     //Function that manages the AI states
     void AIStateController()
     {
+        if (isUtility)
+        {
+            return;
+        }
+        else if (enemyHealthSystem.currentHealth <= 0)
+        {
+            isUtility = true;
+            ChangeToUtility();
+            return;
+        }
         //rounding off the distance to a decimal with 2 places for more accuracy
         distance = Mathf.Round(Vector3.Distance(this.gameObject.transform.position, GameController.instance.toFollow) * 100) / 100;
 
         //if player is enemy's attack range and melee type
         if (distance <= attackRange && !isRanged)
         {
-            currentAction = SpawnerState.MeleeAttack;
-
+            currentAction = State.MeleeAttack;
             enemyMovement.UpdateDirection(GameController.instance.toFollow);
-
             enemyMovement.agent.velocity = Vector3.zero;
             enemyCombat.MeleeAttack();
         }
-
         //if player is in enemy's attack range and ranged type
         else if (distance <= attackRange && isRanged)
         {
@@ -68,7 +109,7 @@ public class EnemyAI : MonoBehaviour
             //Changes enemy's position after each attack, the enemy gets a time limit in which it can move and if it's done moving it will commence the attack
             if (isChangingPosition && timeUntilRangedAttack >= 0)
             {
-                currentAction = SpawnerState.Chasing;
+                currentAction = State.Chasing;
               //  Debug.Log("Changing Position");
                 timeUntilRangedAttack -= Time.deltaTime;
                 enemyMovement.MoveToPlayer(GameController.instance.toFollow, attackRange);
@@ -77,7 +118,7 @@ public class EnemyAI : MonoBehaviour
             //If the time until attack variable is zero then we set the variables required to commence an attack and reset the last enemy explosion count
             else if (timeUntilRangedAttack <= 0)
             {
-                currentAction = SpawnerState.RangedAttack;
+                currentAction = State.RangedAttack;
                // Debug.Log("Done changing position");
                 isChangingPosition = false;
                 enemyCombat.doneAttacking = false;
@@ -87,7 +128,7 @@ public class EnemyAI : MonoBehaviour
             //If enemy is done attacking we change it's state to chasing and provide it with a new offset where the enemy moves
             else if (enemyCombat.doneAttacking)
             {
-                currentAction = SpawnerState.Chasing;
+                currentAction = State.Chasing;
 
                 enemyMovement.GetRandomAngle(enemyMovement.targetOffset);
 
@@ -107,7 +148,7 @@ public class EnemyAI : MonoBehaviour
         //if enemy is not in attack range
         else
         {
-            currentAction = SpawnerState.Chasing;
+            currentAction = State.Chasing;
             enemyCombat.currentCount = 0;
             enemyMovement.MoveToPlayer(GameController.instance.toFollow, attackRange);
             enemyMovement.UpdateDirection(GameController.instance.toFollow);
